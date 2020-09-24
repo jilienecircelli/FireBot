f1e041488de160832783543d24316a26ae0a2c1b
Jiliene Circelli <jilienecircelli@gmail.com>
2020-09-23 20:40:29 -0700

    adding initial files

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const prefix = '-'
+
+client.once('ready', () => {
+    console.log('FireBot is online!');
+});
+
+client.login('NzUwNDM4MjYzMDUwNzMxNTgx.X06iGA.umLus-1JnlUqpmXrnELaBtdq8Bw');
